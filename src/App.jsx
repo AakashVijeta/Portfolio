f14250fafaf7b4be7d4de49b6d96da67e646862c
Aakash Vijeta <aakashvijeta2@gmail.com>
2026-04-18 00:42:14 +0530

    feat: wire SectionManager into App, remove Bootstrap nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,35 @@
-import React, { useEffect, useState } from "react";
-import Intro from "./components/Intro";
-import NavBar from "./components/NavBar";
-import About from "./components/about";
-import "./App.css";
-import Projects from "./components/Projects";
-import Credits from "./components/Credits";
-import StatusChrome from "./components/StatusChrome";
-import Preloader from "./components/Preloader";
-import ProjectDetail from "./components/ProjectDetail";
-import ThemeToggle from "./components/ThemeToggle";
-import PageTransition from "./components/PageTransition";
-import { getProjectBySlug } from "./data/projects";
+import { SectionProvider } from './context/SectionContext';
+import SectionManager from './components/SectionManager';
+import ProjectOverlay from './components/ProjectOverlay';
+import Preloader from './components/Preloader';
+import StatusChrome from './components/StatusChrome';
+import ThemeToggle from './components/ThemeToggle';
+import IntroSection from './components/sections/IntroSection';
+import ProjectsSection from './components/sections/ProjectsSection';
+import AboutSection from './components/sections/AboutSection';
+import ContactSection from './components/sections/ContactSection';
+import CreditsSection from './components/sections/CreditsSection';
+import './App.css';
 
-const parseProjectSlug = (hash) => {
-  const match = hash.match(/^#\/projects\/([\w-]+)/);
-  return match ? match[1] : null;
-};
+const SECTION_COMPONENTS = [
+  IntroSection,
+  ProjectsSection,
+  AboutSection,
+  ContactSection,
+  CreditsSection,
+];
 
 function App() {
-  const [projectSlug, setProjectSlug] = useState(() =>
-    parseProjectSlug(window.location.hash)
-  );
-
-  useEffect(() => {
-    const onHashChange = () => {
-      setProjectSlug(parseProjectSlug(window.location.hash));
-    };
-    window.addEventListener("hashchange", onHashChange);
-    return () => window.removeEventListener("hashchange", onHashChange);
-  }, []);
-
-  const activeProject = projectSlug ? getProjectBySlug(projectSlug) : null;
-
-  const handleBack = () => {
-    window.location.hash = "#projects";
-  };
-
-  if (activeProject) {
-    return (
+  return (
+    <SectionProvider>
       <div className="App">
-        <div className="grain" aria-hidden="true" />
+        <Preloader />
         <StatusChrome />
         <ThemeToggle />
-        <NavBar />
-        <PageTransition key={activeProject.slug}>
-          <ProjectDetail project={activeProject} onBack={handleBack} />
-        </PageTransition>
+        <SectionManager sections={SECTION_COMPONENTS} />
+        <ProjectOverlay />
       </div>
-    );
-  }
-
-  return (
-    <div className="App">
-      <Preloader />
-      <div className="grain" aria-hidden="true" />
-      <StatusChrome />
-      <ThemeToggle />
-      <NavBar />
-      <PageTransition key="home">
-        <div id="content">
-          <Intro />
-          <Projects />
-          <About />
-          <Credits />
-        </div>
-      </PageTransition>
-    </div>
+    </SectionProvider>
   );
 }
 
